9a1e3c99d108f5729c05c94d0e0efa1f83e98925
Tomax-gomez <127521420+Tomax-gomez@users.noreply.github.com>
2024-09-10 14:33:13 -0500

    Add files via upload

---
+++ cesde/src/test/java/com/academico/cesde/CesdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academico.cesde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CesdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
